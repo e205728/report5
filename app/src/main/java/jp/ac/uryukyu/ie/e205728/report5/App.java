4d32ff239edef36e1b1fdd41977990ac6593f2f9
Chen Shude <e205728@ie.u-ryukyu.ac.jp>
2020-12-21 11:19:45 +0900

    step3

--- app/src/main/java/jp/ac/uryukyu/ie/e205728/report5/App.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205728/report5/App.java
@@ -7,16 +7,16 @@ public class App {
    
 
     public static void main(String[] args) {
-        /*String str = null;
+        /*String str = null;      // String型変数 str を宣言し、初期値として null を代入する。
         try {
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerException!");
-            System.out.println(e.getMessage());
+            System.out.println(str.length());    // str.length() の内容を表示する。
+        } catch (NullPointerException e) {         //「NullPointerException」を catch 。
+            System.out.println("NullPointerException!");     // NullPointerExceptionが発生したことを標準出力せよ。
+            System.out.println(e.getMessage());       // 例外インスタンスを用いて、getMessage()を出力せよ。
             //TODO: handle exception
         }
         */
-        String str = "百二十三";
-        int value = Integer.parseInt(str);
+        String str = "百二十三";    // String型の変数strに、文字列"百二十三"を代入
+        int value = Integer.parseInt(str);  // int型変数valueに代入
     }
 }
